564d1ee979a232eff9365509a0e796935552f1ff
Jack Xu <xujack2001@gmail.com>
2020-07-23 11:30:10 -0400

    updated some get methods

---
+++ build/precache-manifest.7f403d75a8b5a5d6c740483cec5ba0d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478fc1233a70c5d522b809fc22c8300a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b0cc9d3982b67804357",
+    "url": "/static/js/2.3b107b5b.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.3b107b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4f3297edc3e8626709",
+    "url": "/static/js/main.37316dac.chunk.js"
+  },
+  {
+    "revision": "a619bde75d8923bbe32b",
+    "url": "/static/js/runtime-main.21ff3829.js"
+  }
+]);
